ea3313006552f2b805ee549f14f8bd222c5e2d87
ishiyamaism <ishiyama@kaizen-keikaku.co.jp>
2024-01-02 09:34:10 +0900

    不要関数の削除

--- utils/db.py
+++ utils/db.py
@@ -14,16 +14,6 @@ db_config = {
 }
 
 
-async def get_db_connection():
-    return await aiomysql.create_pool(
-        user=db_config.get("user"),
-        password=db_config.get("password"),
-        db=db_config.get("database"),
-        host=db_config.get("host"),
-        port=int(db_config.get("port")) if db_config.get("port") else 3306
-    )
-
-
 async def connect_to_db(app: FastAPI):
     app.state.db_pool = await aiomysql.create_pool(
         user=db_config.get("user"),
